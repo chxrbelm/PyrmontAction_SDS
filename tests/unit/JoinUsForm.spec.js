b9b48022e5644c343af299bd35aeb2204d989dc6
Henil <henilp467@gmail.com>
2025-09-23 14:32:58 +1000

    update .gitignore file to avoid pushing e2e playwright test files

---
+++ tests/unit/JoinUsForm.spec.js
@@ -0,0 +1,18 @@
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import JoinUsForm from "../../src/components/JoinUsForm.vue";
+
+describe("JoinUsForm.vue", () => {
+
+});
+
+// describe("Password validation", () => {
+//     it("marks password rules correctly", async () => {
+//         const wrapper = mount(JoinUsForm);
+
+//         // set weak password
+//         await wrapper.find("#password").setValue("weak");
+//         expect(wrapper.vm.passwordValidator.minlength).toBe(false);
+//         expect(wrapper.vm.passwordValidator.uppercase).toBe(false);
+//     });
+// });
